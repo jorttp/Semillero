997ca945783aedeb3bd37014d223def42be93ab4
Jose Manuel Mosquera Restrepo <joserpo2003@gmail.com>
2025-09-23 21:14:00 -0500

    primer commit

---
+++ src/main/java/co/edu/uco/reactiveexample/initiallizer/ReactiveexampleApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.reactiveexample.initiallizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uco/reactiveexample/initiallizer/ReactiveexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.reactiveexample.initiallizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
